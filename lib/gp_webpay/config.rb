3fc57cbca8d1432f5ff23efe3741be984c7bc779
Andrey Ognevsky <ognevsky@me.com>
2016-04-21 14:02:16 +0200

    Change KB to RB

--- lib/gp_webpay/config.rb
+++ lib/gp_webpay/config.rb
@@ -30,9 +30,9 @@ module GpWebpay
 
     def pay_url
       if production?
-        'https://3dsecure.gpwebpay.com/kb/order.do'
+        'https://3dsecure.gpwebpay.com/rb/order.do'
       else
-        'https://test.3dsecure.gpwebpay.com/kb/order.do'
+        'https://test.3dsecure.gpwebpay.com/rb/order.do'
       end
     end
 
